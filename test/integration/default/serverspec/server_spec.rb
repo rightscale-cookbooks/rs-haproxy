b68982838e2347aee282fc66e4b1e477fa103e18
Efrain Olivares <efrain@rightscale.com>
2014-02-12 14:53:40 -0800

    Add gem to parse haproxy.cfg

--- test/integration/default/serverspec/server_spec.rb
+++ test/integration/default/serverspec/server_spec.rb
@@ -4,6 +4,21 @@ describe file("/etc/haproxy/haproxy.cfg") do
   it { should be_file }
 end
 
+config = HAProxy::Config.parse_file('/etc/haproxy/haproxy.cfg')
+
+describe "gem haproxy-tools should be intalled" do
+  describe command("gem list | grep haproxy-tools") do
+    it { should return_stdout /haproxy-tools/  }
+  end
+end
+
+describe "Verify values in config file" do
+  describe "global maxconn" do
+    config.global["maxconn"].should == "4096"
+  end
+end
+
+
 describe service("haproxy") do
   it { should be_enabled }
   it { should be_running }
--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -1,5 +1,12 @@
 require 'serverspec'
 require 'pathname'
+require 'rubygems/dependency_installer'
 
 include Serverspec::Helper::Exec
 include Serverspec::Helper::DetectOS
+
+installer = Gem::DependencyInstaller.new
+installer.install('haproxy-tools')
+Gem.clear_paths
+
+require 'haproxy-tools'
