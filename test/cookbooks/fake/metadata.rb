2a911d20c7247328da1783f69995d2e9b6271e2f
david-vo <david.vo@rightscale.com>
2014-02-12 14:41:57 -0800

    Initial population of backend app servers

---
+++ test/cookbooks/fake/metadata.rb
@@ -0,0 +1,11 @@
+name             'fake'
+maintainer       'RightScale, Inc.'
+maintainer_email 'cookbooks@rightscale.com'
+license          'Apache 2.0'
+description      'Installs/Configures a test haproxy server'
+version          '0.1.0'
+
+depends 'rs-haproxy'
+depends 'machine_tag'
+
+recipe 'fake::default', 'Tags servers to be used in the haproxy backend'
