4d501ec68f19306c5188f840b5acf268aacab84d
david-vo <david.vo@rightscale.com>
2014-02-14 14:31:50 -0800

    Add some more tests

--- test/integration/default/serverspec/server_spec.rb
+++ test/integration/default/serverspec/server_spec.rb
@@ -13,14 +13,26 @@ describe "Verify settings in haproxy.cfg file" do
     { "global" => "group haproxy" },
     { "global" => "stats socket\s+/var/run/haproxy\.sock user haproxy group haproxy" },
     { "defaults" => "log\s+global" },
-    { "defaults" => "mode\s+http" }, 
-    { "backend default" => "mode http"}
-  ].each do |pair| 
+    { "defaults" => "mode\s+http" },
+    { "defaults" => "balance\s+roundrobin" },
+    { "defaults" => "stats uri /haproxy-status" },
+    { "defaults" => "http-check disable-on-404" }
+  ].each do |pair|
     pair.each do |group, setting|
       it "#{group} should contain #{setting}" do
-        find_haproxy_setting(config_file, /#{group}/, /#{setting}/).should == TrueClass 
+        find_haproxy_setting(config_file, /#{group}/, /#{setting}/).should == TrueClass
       end
     end
+  end
+end
+
+describe "The proper user and group should exist on the server" do
+  describe user('haproxy') do
+    it { should exist }
+  end
+
+  describe user('haproxy') do
+    it { should belong_to_group 'haproxy' }
   end
 end
 
