c9a3fd29a669215548120959c9549a3afab08ba0
david-vo <david.vo@rightscale.com>
2014-02-14 16:49:44 -0800

    Add more tests

--- test/cookbooks/fake/recipes/add_apps_to_pools.rb
+++ test/cookbooks/fake/recipes/add_apps_to_pools.rb
@@ -28,7 +28,7 @@ app1_host1_tags = [
   "application:active_app1=true",
   "application:bind_ip_address_app1=10.1.55.22",
   "application:bind_port_app1=80",
-  "application:vhost_path_app1=site.com"
+  "application:vhost_path_app1=alpha.com"
 ]
 
 ::FileUtils.mkdir_p(app1_host1_dir)
@@ -43,7 +43,7 @@ app1_host2_tags = [
   "application:active_app1=true",
   "application:bind_ip_address_app1=10.1.55.33",
   "application:bind_port_app1=80",
-  "application:vhost_path_app1=site.com"
+  "application:vhost_path_app1=beta.com"
 ]
 
 ::FileUtils.mkdir_p(app1_host2_dir)
@@ -58,11 +58,26 @@ app2_host1_tags = [
   "application:active_app2=true",
   "application:bind_ip_address_app2=10.1.66.22",
   "application:bind_port_app2=80",
-  "application:vhost_path_app2=site.com"
+  "application:vhost_path_app2=charlie.com"
 ]
 
 ::FileUtils.mkdir_p(app2_host1_dir)
 ::File.open(::File.join(app2_host1_dir, 'tags.json'), 'w') { |file| file.write(::JSON.pretty_generate(app2_host1_tags)) }
+
+app3_host1_dir = "/vagrant/cache_dir/machine_tag_cache/app3-host1"
+
+# Tags for server1 on the app2 pool
+app3_host1_tags = [
+  "server:uuid=app3host1",
+  "application:active=true",
+  "application:active_app3=true",
+  "application:bind_ip_address_app3=10.1.77.44",
+  "application:bind_port_app3=80",
+  "application:vhost_path_app3=delta.com"
+]
+
+::FileUtils.mkdir_p(app3_host1_dir)
+::File.open(::File.join(app3_host1_dir, 'tags.json'), 'w') { |file| file.write(::JSON.pretty_generate(app3_host1_tags)) }
 
 default_folder = "/vagrant/cache_dir/machine_tag_cache/default"
 
--- test/integration/default/serverspec/server_spec.rb
+++ test/integration/default/serverspec/server_spec.rb
@@ -16,7 +16,12 @@ describe "Verify settings in haproxy.cfg file" do
     { "defaults" => "mode\s+http" },
     { "defaults" => "balance\s+roundrobin" },
     { "defaults" => "stats uri /haproxy-status" },
-    { "defaults" => "http-check disable-on-404" }
+    { "defaults" => "http-check disable-on-404" },
+    { "backend app1" => "server app1host2 10.1.55.33:80 inter 300 rise 2 fall 3 maxconn 100 cookie app1host2" },
+    { "backend app1" => "server app1host1 10.1.55.22:80 inter 300 rise 2 fall 3 maxconn 100 cookie app1host1" },
+    { "backend app2" => "server app2host1 10.1.66.22:80 inter 300 rise 2 fall 3 maxconn 100 cookie app2host1" },
+    { "backend app3" => "server app3host1 10.1.77.44:80 inter 300 rise 2 fall 3 maxconn 100 cookie app3host1" },
+    { "backend default" => "server discourse 166.78.170.64:80 inter 300 rise 2 fall 3 maxconn 100 cookie discourse" }
   ].each do |pair|
     pair.each do |group, setting|
       it "#{group} should contain #{setting}" do
@@ -46,7 +51,7 @@ describe service("haproxy") do
 end
 
 describe "Should return the correct Discourse webpage greeting" do
-  describe command('curl 33.33.33.5/login | grep "Welcome"') do
-    it { should return_stdout 'Welcome to RightScale Rightscale Discourse'}
+  describe command('curl 33.33.33.5/login | grep -o "Welcome to RightScale RightScale Discourse"') do
+    it { should return_stdout 'Welcome to RightScale RightScale Discourse'}
   end
 end
