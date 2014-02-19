5ee67bce7bf7db309cfd5c1059d07116b38f881c
Efrain Olivares <efrain@rightscale.com>
2014-02-19 14:41:52 -0800

    Use array of pairs instead of hashes for settings test.  Add test for haproxy socket

--- test/integration/default/serverspec/server_spec.rb
+++ test/integration/default/serverspec/server_spec.rb
@@ -6,27 +6,34 @@ describe file(config_file) do
   it { should be_file }
 end
 
+describe file('/var/run/haproxy.sock') do
+  it { should be_socket }
+end
+
+
+# Helper function find_haproxy_setting takes three arguments.
+# Name of cfg file, group name, setting under group.  (See spec_helper file)
+# Function returns true if regex matching setting is found in that group.
+# Array of pairs is used to avoid nested hashes, or a workaround for duplicate keys in the hashes.
 describe "Verify settings in haproxy.cfg file" do
   [
-    { "global" => "maxconn 4096" },
-    { "global" => "user haproxy" },
-    { "global" => "group haproxy" },
-    { "global" => "stats socket\s+/var/run/haproxy\.sock user haproxy group haproxy" },
-    { "defaults" => "log\s+global" },
-    { "defaults" => "mode\s+http" },
-    { "defaults" => "balance\s+roundrobin" },
-    { "defaults" => "stats uri /haproxy-status" },
-    { "defaults" => "http-check disable-on-404" },
-    { "backend app1" => "server app1host2 10.1.55.33:80 inter 300 rise 2 fall 3 maxconn 100 cookie app1host2" },
-    { "backend app1" => "server app1host1 10.1.55.22:80 inter 300 rise 2 fall 3 maxconn 100 cookie app1host1" },
-    { "backend app2" => "server app2host1 10.1.66.22:80 inter 300 rise 2 fall 3 maxconn 100 cookie app2host1" },
-    { "backend app3" => "server app3host1 10.1.77.44:80 inter 300 rise 2 fall 3 maxconn 100 cookie app3host1" },
-    { "backend default" => "server discourse 166.78.170.64:80 inter 300 rise 2 fall 3 maxconn 100 cookie discourse" }
+    [ "global",  "maxconn 4096" ],
+    [ "global",  "user haproxy" ],
+    [ "global",  "group haproxy" ],
+    [ "global",  "stats socket\s+/var/run/haproxy\.sock user haproxy group haproxy" ],
+    [ "defaults",  "log\s+global" ],
+    [ "defaults",  "mode\s+http" ],
+    [ "defaults",  "balance\s+roundrobin" ],
+    [ "defaults",  "stats uri /haproxy-status" ],
+    [ "defaults",  "http-check disable-on-404" ],
+    [ "backend app1",  "server app1host2 10.1.55.33:80 inter 300 rise 2 fall 3 maxconn 100 cookie app1host2" ],
+    [ "backend app1",  "server app1host1 10.1.55.22:80 inter 300 rise 2 fall 3 maxconn 100 cookie app1host1" ],
+    [ "backend app2",  "server app2host1 10.1.66.22:80 inter 300 rise 2 fall 3 maxconn 100 cookie app2host1" ],
+    [ "backend app3",  "server app3host1 10.1.77.44:80 inter 300 rise 2 fall 3 maxconn 100 cookie app3host1" ],
+    [ "backend default",  "server discourse 166.78.170.64:80 inter 300 rise 2 fall 3 maxconn 100 cookie discourse" ]
   ].each do |pair|
-    pair.each do |group, setting|
-      it "#{group} should contain #{setting}" do
-        find_haproxy_setting(config_file, /#{group}/, /#{setting}/).should == TrueClass
-      end
+    it "#{pair.first} should contain #{pair.last}" do
+        find_haproxy_setting(config_file, pair.first, pair.last).should == true 
     end
   end
 end
--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -5,23 +5,28 @@ include Serverspec::Helper::Exec
 include Serverspec::Helper::DetectOS
 
 # Helper function to sort through the haproxy.cfg
-# returns true if setting is found under give group.
+# 
+# @param config_file the name of the config file (/var/haproxy/haproxy.cfg)
+# @param regex_group is the section name in the haproxy config (global, default, ....)
+# @param regex_setting is a string used as a regex to match setting underl group
+# return true if found, false otherwise.
+#
 def find_haproxy_setting(config_file,  regex_group, regex_setting)
   hacfg = IO.readlines( config_file )
   hacfg = hacfg.reject! { |line|  line =~ /^#/ || line =~ /^\s*#/  || line =~ /^\s*$/ }
   
   i = 0 
   while i < hacfg.length() do
-    if hacfg[i] =~ /^\S/ and hacfg[i] =~ regex_group
+    if hacfg[i] =~ /^\S/ and hacfg[i] =~ /#{regex_group}/
         i += 1
         while hacfg[i] =~ /^\s/ do
-           if hacfg[i] =~ regex_setting 
-              return TrueClass 
+           if hacfg[i] =~ /#{regex_setting}/ 
+              return true 
            end 
            i += 1
         end 
     end 
     i += 1
   end 
-  return FalseClass
+  return false
 end
