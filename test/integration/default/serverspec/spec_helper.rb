302f6a9615ff1defc9f8e53a81eb908d70e723bd
Efrain Olivares <efrain@rightscale.com>
2014-02-20 14:44:53 -0800

    Updating comments for clarification on the haproxy_stat helper function.

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -36,7 +36,7 @@ end
 
 # Helper function to sort through haproxy socket info.
 #
-# @param regex_setting is the setting we want to look the value for.
+# @param regex_setting [String] is the setting we want to look the value for.
 # returns the value of the parameter
 #
 # This function reads the haproxy socket.  It parses through the info section
@@ -64,13 +64,15 @@ end
 
 # Helper function to sort through haproxy socket info.
 #
-# @param regex_setting is the setting we want to look the value for.
-# returns the value of the parameter
+# @param pxname [String] the first value in row we want to select.
+# @param svname [String] the second value in the row we want to select.
+# @param column [String] the name of the column to select from
+# returns the value found at the selected row and column.
 #
 # This function reads the haproxy socket.  It parses through the info section
-# and puts the data into a csv format, from which we can request values
-# given the parameter name.
-def haproxy_stat( regex_pxname, regex_svname, regex_query )
+# and puts the data into a csv format.  The row is selected by providing the 
+# first two values in the row.  The colum is slected by name.
+def haproxy_stat( pxname, svname, column )
 
   begin
     socket = UNIXSocket.new('/var/run/haproxy.sock')
@@ -85,10 +87,10 @@ def haproxy_stat( regex_pxname, regex_svname, regex_query )
   end
   
   csv_content = CSV.parse(content)
-  index  = csv_content[0].index("#{regex_query}")
+  index  = csv_content[0].index("#{column}")
 
   csv_content.each do |line|
-    if line[0] =~ /#{regex_pxname}/i and line[1] =~ /#{regex_svname}/ 
+    if line[0] =~ /#{pxname}/i and line[1] =~ /#{svname}/ 
      return line[index].strip()
     end
   end
