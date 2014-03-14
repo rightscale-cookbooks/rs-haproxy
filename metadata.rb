be059affcff7ffdce01c91d04b89c67725722dd4
Nitin Mohan <nitin@rightscale.com>
2014-03-14 14:14:51 -0700

    Bump version to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'cookbooks@rightscale.com'
 license          'Apache 2.0'
 description      'Application cookbook to set up HAProxy on a RightScale environment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 depends 'marker', '~> 1.0.0'
-depends 'haproxy'
+depends 'haproxy', '~> 1.6.0'
 depends 'collectd', '~> 1.1.0'
 depends 'rightscale_tag', '~> 1.0.1'
 
