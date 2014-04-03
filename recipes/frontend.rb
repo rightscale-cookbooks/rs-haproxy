1cc81bb4bb1339760eeebdb0fde8282610a840d1
Nitin Mohan <nitin@rightscale.com>
2014-04-02 17:22:47 -0700

    Fix frontend recipe not generating default backend section

--- recipes/frontend.rb
+++ recipes/frontend.rb
@@ -64,31 +64,40 @@ unless node['remote_recipe'].nil? || node['remote_recipe'].empty?
   node.set['remote_recipe'] = nil
 end
 
+# Initialize frontend section which will be generated in the haproxy.cfg
 node.set['haproxy']['config']['frontend'] = {}
 node.set['haproxy']['config']['frontend']['all_requests'] ||= {}
 node.set['haproxy']['config']['frontend']['all_requests']['default_backend'] = node['rs-haproxy']['pools'].last
 
+# Initialize backend section which will be generated in the haproxy.cfg
 node.set['haproxy']['config']['backend'] = {}
 
+# Iterate through each application server pool served by the HAProxy server and set up the
+# ACLs in the frontend section and the corresponding backed sections
 node['rs-haproxy']['pools'].each do |pool_name|
-  # If there exists application servers with application name same as pool name, setup ACLs
-  # and backend for the pool.
+  backend_servers_list = []
+
+  if node['rs-haproxy']['session_stickiness']
+    # When cookie is enabled the haproxy.cnf should have this dummy server
+    # entry for the haproxy to start without any errors
+    backend_servers_list << {'disabled-server 127.0.0.1:1' => {'disabled' => true}}
+  end
+
+  # If there exists application servers with application name same as pool name add those
+  # servers to the corresponding backend section in the haproxy.cfg. Also, set up the ACLs
+  # based on the vhost_path information in the application server.
   unless app_server_pools[pool_name].nil?
-    node.set['haproxy']['config']['backend'][pool_name] = {}
-
     acl_setting = ''
-    backend_servers_list = []
-
     app_server_pools[pool_name].each do |server_uuid, server_hash|
       if server_hash['vhost_path'].include?('/')
         # If vhost_path contains a '/' then the ACL should match the path in the request URI.
-        # e.g., if the request uri is www.example.com/index then the ACL will match '/index'
+        # e.g., if the request URI is www.example.com/index then the ACL will match '/index'
         acl_setting = "path_dom -i #{server_hash['vhost_path']}"
       else
-        # Else the ACL should match the domain name in the host name of the request URI.
+        # Else the ACL should match the domain name of the request URI.
         # e.g., if the request URI is http://test.example.com then the ACL will
-        # match 'test.example.com'
-        # if the request URI is http://example.com then the ACL will match 'example.com'
+        # match 'test.example.com' and if the request URI is http://example.com
+        # then the ACL will match 'example.com'
         acl_setting = "hdr_dom(host) -i -m dom #{server_hash['vhost_path']}"
       end
 
@@ -106,24 +115,24 @@ node['rs-haproxy']['pools'].each do |pool_name|
 
       # Configure cookie for backend server
       if node['rs-haproxy']['session_stickiness']
-        # When cookie is enabled the haproxy.cnf should have this dummy server
-        # entry for the haproxy to start without any errors
-        backend_servers_list << {'disabled-server 127.0.0.1:1' => {'disabled' => true}}
         backend_server_hash['cookie'] = backend_server.split(' ').first
       end
 
       backend_servers_list << {backend_server => backend_server_hash}
     end
 
+    # Set up ACLs based on the vhost_path information from the application servers
     acl_name = "acl_#{pool_name}"
     node.set['haproxy']['config']['frontend']['all_requests']['acl'] ||= {}
     node.set['haproxy']['config']['frontend']['all_requests']['acl'][acl_name] = acl_setting
     node.set['haproxy']['config']['frontend']['all_requests']['use_backend'] ||= {}
     node.set['haproxy']['config']['frontend']['all_requests']['use_backend'][pool_name] = "if #{acl_name}"
-
-    node.set['haproxy']['config']['backend'][pool_name]['server'] ||= []
-    node.set['haproxy']['config']['backend'][pool_name]['server'] = backend_servers_list
   end
+
+  # Set up backend section for each application server pool served by HAProxy
+  node.set['haproxy']['config']['backend'][pool_name] = {}
+  node.set['haproxy']['config']['backend'][pool_name]['server'] ||= []
+  node.set['haproxy']['config']['backend'][pool_name]['server'] = backend_servers_list
 end
 
 include_recipe 'rs-haproxy::default'
