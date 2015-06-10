ca24fd20b8c80c89d9910f3c9831542081c6251e
Richard Shade <rshade@rightscale.com>
2015-06-10 15:46:14 -0500

    adding chefspec coverage to default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,7 @@ $LOAD_PATH.unshift(libraries_path) unless $LOAD_PATH.include?(libraries_path)
 
 require 'chefspec'
 require 'chefspec/berkshelf'
+ChefSpec::Coverage.start!
 
 RSpec.configure do |config|
   config.platform = 'ubuntu'
