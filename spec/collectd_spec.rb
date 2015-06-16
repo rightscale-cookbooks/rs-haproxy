35038a8fb8c8f4873e3ee25eee6cc4ee4af35ddc
Richard Shade <rshade@rightscale.com>
2015-06-16 15:31:19 -0500

    adding collectd_spec to start test coverage

---
+++ spec/collectd_spec.rb
@@ -0,0 +1,28 @@
+require_relative 'spec_helper'
+
+describe 'rs-haproxy::collectd' do
+  let(:chef_run) do
+    ChefSpec::Runner.new do |node|
+      node.set['rightscale']['instance_uuid'] = 'abcd1234'
+    end.converge(described_recipe)
+  end
+
+  context 'installing collectd and setting up haproxy plugin' do
+    it 'installs chef_rewind' do
+      expect(chef_run).to install_chef_gem('chef-rewind')
+    end
+
+    it 'includes collectd default recipe' do
+      expect(chef_run).to include_recipe('collectd::default')
+    end
+
+    it 'installs socat package' do
+      expect(chef_run).to install_package('socat')
+    end
+
+    it '# Put the haproxy collectd plugin script into the collectd lib directory' do
+      expect(chef_run).to create_cookbook_file("#{chef_run.node['collectd']['plugin_dir']}/haproxy")
+      expect(chef_run).to create_cookbook_file('/usr/share/collectd/haproxy.db')
+    end
+  end
+end
