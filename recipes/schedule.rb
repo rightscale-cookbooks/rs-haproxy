323374cab3253a0b2ab1388a6cf977c17438a2f8
Curt Wilhelm <curt@rightscale.com>
2016-01-07 13:15:16 -0700

    create a unique file for the schedule so it doesn't get overwritten from other chef calls

--- recipes/schedule.rb
+++ recipes/schedule.rb
@@ -29,6 +29,7 @@ interval = node['rs-haproxy']['schedule']['interval']
 
 chef_dir='/home/rightscale/.chef'
 chef_file = "#{chef_dir}/chef.json"
+chef_schedule_file = "#{chef_dir}/schedule.json"
 chef_json={}
 
 # read the existing chef.json file 
@@ -43,7 +44,7 @@ ruby_block "read chef json" do
       chef_json.delete("remote_recipe") 
       chef_json.delete('rs-haproxy')
       chef_json.merge!("run_list"=>["recipe[rs-haproxy::frontend]"])
-      File.write(chef_file, ::JSON.pretty_generate(chef_json))
+      File.write(chef_schedule_file, ::JSON.pretty_generate(chef_json))
     end
   end
 end
@@ -53,6 +54,6 @@ cron "rs-haproxy::frontend" do
   user 'rightscale'
   minute "*/#{interval}"
   hour '*'
-  command "sudo chef-client --json-attributes #{chef_file} --config #{chef_dir}/client.rb"
+  command "sudo chef-client --json-attributes #{chef_schedule_file} --config #{chef_dir}/client.rb"
   action schedule_enable ? :create : :delete
 end
