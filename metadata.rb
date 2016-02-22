907dfed0f2ec533cf226e7dba0c837668f41c4bc
tusharshahrs <tushar.shah@rightscale.com>
2016-02-22 08:22:40 -0500

    updated true

--- metadata.rb
+++ metadata.rb
@@ -191,7 +191,7 @@ attribute "rs-haproxy/force_ssl_redirect",
   :description => "Redirect all HTTP traffic to HTTPS when SSL is handled by haproxy.",
   :required => "optional",
   :choice => ["true", "false"],
-  :default => "false
+  :default => "false"
 #  :choice => [ :yesforce => true, :noforce => false],
  # :default => :yesforce
 
