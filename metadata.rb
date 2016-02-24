0287856f4224bcbec4ab81995c0abe8a28a02af4
tusharshahrs <tushar.shah@rightscale.com>
2016-02-24 14:48:11 -0500

    removed type

--- metadata.rb
+++ metadata.rb
@@ -197,5 +197,4 @@ attribute "rs-haproxy/acl_list_for_https_exclusion",
   :display_name => "acl list for https exclusion from force_ssl_redirect",
   :description => "Paths to exclude from force ssl. e.g.:  /web.*  /web2/test1.*  The .* required to get everything afterwards.  Please do NOT put a command between the paths",
   :required => "optional",
-  :type => 'string',
   :default => ''
