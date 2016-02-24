fa5b97a0d7bc033b42df309a633389b128f48411
tusharshahrs <tushar.shah@rightscale.com>
2016-02-24 14:09:03 -0500

    single quotes

--- metadata.rb
+++ metadata.rb
@@ -198,4 +198,4 @@ attribute "rs-haproxy/acl_list_for_https_exclusion",
   :description => "Paths to exclude from force ssl. e.g.:  /web.*  /web2/test1.*  The .* required to get everything afterwards.  Please do NOT put a command between the paths",
   :required => "optional",
   :type => 'string'
-  :default => ""
+  :default => ''
