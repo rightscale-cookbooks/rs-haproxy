4f11555e32eb866123c2e47c3eef3f3d404d7961
Bryan Karaffa <bryankaraffa@gmail.com>
2016-12-06 07:44:26 -0800

    Update metadata.rb
    Updated patch version for this edit

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@rightscale.com'
 license          'Apache 2.0'
 description      'Application cookbook to set up HAProxy on a RightScale environment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.3'
+version          '1.2.4'
 
 depends 'marker', '~> 1.0.1'
 depends 'haproxy', '~> 1.6.0'
