3a1cdde2d223cdf2d052b6fb172b6ac92e994997
Richard Shade <rshade@rightscale.com>
2017-01-18 16:30:06 -0600

    fixing test for cookstyle

--- test/integration/backend/serverspec/spec_helper.rb
+++ test/integration/backend/serverspec/spec_helper.rb
@@ -21,7 +21,7 @@ require 'machine_tag'
 # return true if found, false otherwise.
 #
 def find_haproxy_setting(config_file, regex_group, regex_setting)
-  raise "file missing" unless ::File.exist?(config_file)
+  raise 'file missing' unless ::File.exist?(config_file)
   hapcfg = IO.readlines(config_file)
   # puts hapcfg.length
   hapcfg.reject! { |line| line =~ /^#/ || line =~ /^\s*#/ || line =~ /^\s*$/ }
@@ -39,7 +39,7 @@ def find_haproxy_setting(config_file, regex_group, regex_setting)
       return true
     end
   end
-  
+
   false
 end
 
